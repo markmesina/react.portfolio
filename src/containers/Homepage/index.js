404f9c171681eaae71220dcd7b233cee973a22d0
ceress <markmesina69@gmail.com>
2020-06-26 18:27:30 -0700

    finalize template

--- src/containers/Homepage/index.js
+++ src/containers/Homepage/index.js
@@ -1,307 +1,54 @@
-import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import {
-  Button,
-  Container,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  List,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-  Visibility,
-} from 'semantic-ui-react';
-import Headshot from './../../components/Headshot'
-import './style.css';
-const getWidth = () => {
-  const isSSR = typeof window === 'undefined'
+import { Grid, Cell } from 'react-mdl';
 
-  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
-}
-
-/* eslint-disable react/no-multi-comp */
-/* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
- * such things.
- */
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      className = 'shadow'
-      as='h1'
-      content='Mark Mesina'
-      inverted
-      style={{
-          fontSize: mobile ? '2em' : '4em',
-          fontWeight: 'normal',
-          marginBottom: 0,
-          marginTop: mobile ? '1.5em' : '3em',
-        }}>
-
-      </Header>
-        <Headshot/>
-    <Header
-      as='h2'
-      content='Full Stack Web Engineer'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    <Button primary size='huge'>
-      See Biography
-      <Icon name='right arrow' />
-    </Button>
-  </Container>
-)
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-}
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
-
+class Homepage extends Component {
   render() {
-    const { children } = this.props
-    const { fixed } = this.state
+    return(
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C5603AQGzZz8D6dqW6A/profile-displayphoto-shrink_400_400/0?e=1598486400&v=beta&t=2RA6I_c9KKoQ0_-D0AWeXUfHMnICPWms2yWw092dGzQ"
+              alt="headshot"
+              className="avatar-img"
+              />
 
-    return (
-      <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
-        <Visibility
-          once={false}
-          onBottomPassed={this.showFixedMenu}
-          onBottomPassedReverse={this.hideFixedMenu}
-        >
-          <Segment
-            inverted
-            textAlign='center'
-            className = 'landingImage'
-            style={{ minHeight: '100vh', padding: '1em 0em' }}
-            vertical
-          >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              <Container>
-                <Menu.Item as={Link} to= '/' active>
-                  Home
-                </Menu.Item>
-                <Menu.Item as= {Link} to = '/biography'>Biography</Menu.Item>
-                <Menu.Item as= {Link} to = '/contact'>Contact</Menu.Item>
-                <Menu.Item as= {Link} to = '/projects'>Projects</Menu.Item>
-                <Menu.Item position='right'>
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
-          </Segment>
-        </Visibility>
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
 
-        {children}
-      </Responsive>
-    )
-  }
-}
+            <hr/>
 
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
+          <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
 
-class MobileContainer extends Component {
-  state = {}
+        <div className="social-links">
 
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+          {/* LinkedIn */}
+          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-linkedin-square" aria-hidden="true" />
+          </a>
 
-  handleToggle = () => this.setState({ sidebarOpened: true })
+          {/* Github */}
+          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-github-square" aria-hidden="true" />
+          </a>
 
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
+          {/* Freecodecamp */}
+          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-free-code-camp" aria-hidden="true" />
+          </a>
 
-    return (
-      <Responsive
-        as={Sidebar.Pushable}
-        getWidth={getWidth}
-        maxWidth={Responsive.onlyMobile.maxWidth}
-      >
-        <Sidebar
-          as={Menu}
-          animation='push'
-          inverted
-          onHide={this.handleSidebarHide}
-          vertical
-          visible={sidebarOpened}
-        >
-          <Menu.Item as='a' active>
-            Home
-          </Menu.Item>
-          <Menu.Item as='a'>Biography</Menu.Item>
-          <Menu.Item as='a'>Contact</Menu.Item>
-          <Menu.Item as='a'>Projects</Menu.Item>
-        </Sidebar>
+          {/* Youtube */}
+          <a href="http://google.com" rel="noopener noreferrer" target="_blank">
+            <i className="fa fa-youtube-square" aria-hidden="true" />
+          </a>
 
-        <Sidebar.Pusher dimmed={sidebarOpened}>
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 350, padding: '1em 0em' }}
-            vertical
-          >
-            <Container>
-              <Menu inverted pointing secondary size='large'>
-                <Menu.Item onClick={this.handleToggle}>
-                  <Icon name='sidebar' />
-                </Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted>
-                    Home
-                  </Button>
-                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                    Projects
-                  </Button>
-                </Menu.Item>
-              </Menu>
-            </Container>
-            <HomepageHeading mobile />
-          </Segment>
-
-          {children}
-        </Sidebar.Pusher>
-      </Responsive>
-    )
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const ResponsiveContainer = ({ children }) => (
-  <div>
-    <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </div>
-)
-
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const Homepage = () => (
-  <ResponsiveContainer>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Grid container stackable verticalAlign='middle'>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              Problem Solving done right!
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-             A critical thinker that sees situations in conventional and pragmatic angles to efficiently resolve problems
-            </p>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              Bringing back the fun in Coding
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Coding is more than just a career, it is also a passion!
-            </p>
-          </Grid.Column>
-          <Grid.Column floated='right' width={6}>
-            <Image bordered rounded size='large' src='https://sdtimes.com/wp-content/uploads/2014/08/codingisfun.gif' />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign='center'>
-            <Button size='huge'>Check Out These Apps</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: '0em' }} vertical>
-      <Grid celled='internally' columns='equal' stackable>
-        <Grid.Row textAlign='center'>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "Animal Lover!"
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>A little something about me :)</p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "If you can dream it, you can do it!"
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='https://iabtechlab.com/wp-content/uploads/2019/12/walt-disney-logo-transparent-3-1024x630.png' />
-             Walt Disney
-            </p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      <Container text>
-        <Header as='h3' style={{ fontSize: '2em' }}>
-          Breaking The Grid, Grabs Your Attention
-        </Header>
-        <Image small src='https://pbs.twimg.com/media/DkPboApV4AEzyxo.jpg'/>
-
-      </Container>
-    </Segment>
-
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
-      <Container>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About' />
-              <List link inverted>
-                <List.Item as='a'>Sitemap</List.Item>
-                <List.Item as='a'>Contact Us</List.Item>
-                <List.Item as='a'>Religious Ceremonies</List.Item>
-                <List.Item as='a'>Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as='h4' inverted>
-                Footer Header
-              </Header>
-              <p>
-                Extra space for a call to action inside the footer that could help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
+        </div>
+            </div>
+          </Cell>
         </Grid>
-      </Container>
-    </Segment>
-  </ResponsiveContainer>
-)
+      </div>
+    )
+  }
+}
 
-export default Homepage
+export default Homepage;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from "react-dom";
 import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { BrowserRouter } from 'react-router-dom';
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+, document.getElementById("root"));
+
+registerServiceWorker();
