7c461f2a2e8af17d9fe080ddc61536ff80f69494
ceress <markmesina69@gmail.com>
2020-06-27 20:43:36 -0700

    added more text

--- src/containers/Biography/index.js
+++ src/containers/Biography/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { Grid, Cell } from 'react-mdl';
 
 
 class Biography extends Component {
   render() {
     return(
-      <div><h1>Biography Page</h1></div>
+      <div><h1>Biography</h1>
+        <div>
+          <Grid className="demo-grid-1">
+            <Cell col={4}>
+              <h2> About Me </h2>
+            </Cell>
+            <Cell col={4}>
+              <h2> Education </h2>
+            </Cell>
+            <Cell col={4}>
+              <h2> Work Experience </h2>
+            </Cell>
+          </Grid>
+        </div>
+      </div>
     )
   }
 }
